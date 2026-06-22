c5f740b4976c439a3eeb79beb97c90c6ab31b559
Nattawat Upara <thetree102cm@gmail.com>
2026-06-22 16:02:14 +0700

    feat: add hello world program

---
+++ week_01/ex01_hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("สวัสดีครับ!!! ,ผมชื่อ นัทธวัฒน์ อุประ ครับ\nรหัสนักศึกษา : 692110148\nตอนนี้กำลังเรียน\nวิชา : DG111 Computer Programming\n");
+    return 0;
+}
